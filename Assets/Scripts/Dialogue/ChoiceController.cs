8e1194a083c0e11d4b86851863b611e5ea4142ef
Alayna Garry <77999316+AlaynaGarry@users.noreply.github.com>
2022-10-10 15:01:57 -0600

    Week 1 Wrap-up/ Week 2 Start
    Week 2: Choices button UI
    Choices Script

---
+++ Assets/Scripts/Dialogue/ChoiceController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceController : MonoBehaviour
+{
+    [SerializeField] public GameObject choiceButton;
+    [SerializeField] public GameObject choicePanel;
+
+
+}
+
